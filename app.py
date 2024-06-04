70a62aef4966fbef8a2baf12467ddeb32fb29539
Diego Carmona Bernal <cbdiegox@gmail.com>
2024-06-04 11:41:24 -0600

    update

--- app.py
+++ app.py
@@ -6,67 +6,66 @@ app = Flask(__name__)
 
 def syntactic_analysis(tokens):
     index = 0
-    print(tokens)
 
     def parse_expression(tokens):
         nonlocal index
-        if tokens[index][0] == 'STRING':
+        if index < len(tokens) and tokens[index][0] == 'STRING':
             index += 1
-            if tokens[index][0] == 'OP':
+            if index < len(tokens) and tokens[index][0] == 'OP':
                 index += 1
-                if tokens[index][0] == 'NUM':
+                if index < len(tokens) and tokens[index][0] == 'NUM':
                     index += 1
                     return True
         return False
 
     def parse_print(tokens):
         nonlocal index
-        if tokens[index][0] == 'SYSTEM':
+        if index < len(tokens) and tokens[index][0] == 'SYSTEM':
             index += 1
-            if tokens[index][0] == 'PARENIZQ':
+            if index < len(tokens) and tokens[index][0] == 'PARENIZQ':
                 index += 1
                 if parse_expression(tokens):
-                    if tokens[index][0] == 'PARENDER':
+                    if index < len(tokens) and tokens[index][0] == 'PARENDER':
                         index += 1
-                        if tokens[index][0] == 'PUNTOCOMA':
+                        if index < len(tokens) and tokens[index][0] == 'PUNTOCOMA':
                             index += 1
                             return True
         return False
 
     def parse_for(tokens):
         nonlocal index
-        if tokens[index][0] == 'FOR':
+        if index < len(tokens) and tokens[index][0] == 'FOR':
             index += 1
-            if tokens[index][0] == 'PARENIZQ':
+            if index < len(tokens) and tokens[index][0] == 'PARENIZQ':
                 index += 1
-                if tokens[index][0] == 'INT':
+                if index < len(tokens) and tokens[index][0] == 'INT':
                     index += 1
-                    if tokens[index][0] == 'ID':
+                    if index < len(tokens) and tokens[index][0] == 'ID':
                         index += 1
-                        if tokens[index][0] == 'EQ':
+                        if index < len(tokens) and tokens[index][0] == 'EQ':
                             index += 1
-                            if tokens[index][0] == 'NUM':
+                            if index < len(tokens) and tokens[index][0] == 'NUM':
                                 index += 1
-                                if tokens[index][0] == 'PUNTOCOMA':
+                                if index < len(tokens) and tokens[index][0] == 'PUNTOCOMA':
                                     index += 1
-                                    if tokens[index][0] == 'ID':
+                                    if index < len(tokens) and tokens[index][0] == 'ID':
                                         index += 1
-                                        if tokens[index][0] == 'OP':
+                                        if index < len(tokens) and tokens[index][0] == 'OP':
                                             index += 1
-                                            if tokens[index][0] == 'NUM':
+                                            if index < len(tokens) and tokens[index][0] == 'NUM':
                                                 index += 1
-                                                if tokens[index][0] == 'PUNTOCOMA':
+                                                if index < len(tokens) and tokens[index][0] == 'PUNTOCOMA':
                                                     index += 1
-                                                    if tokens[index][0] == 'ID':
+                                                    if index < len(tokens) and tokens[index][0] == 'ID':
                                                         index += 1
-                                                        if tokens[index][0] == 'OP':
+                                                        if index < len(tokens) and tokens[index][0] == 'OP':
                                                             index += 1
-                                                            if tokens[index][0] == 'PARENDER':
+                                                            if index < len(tokens) and tokens[index][0] == 'PARENDER':
                                                                 index += 1
-                                                                if tokens[index][0] == 'LLAVEIZQ':
+                                                                if index < len(tokens) and tokens[index][0] == 'LLAVEIZQ':
                                                                     index += 1
                                                                     if parse_print(tokens):
-                                                                        if tokens[index][0] == 'LLAVEDER':
+                                                                        if index < len(tokens) and tokens[index][0] == 'LLAVEDER':
                                                                             return "Estructura FOR correcta"
         return "Error linea: " + str(index) + " con el token: " + str(tokens[index])
 
@@ -79,7 +78,7 @@ def lexical_analysis(code):
         ('FOR', r'\bfor\b'),
         ('INT', r'\bint\b'),
         ('PRINTLN', r'\bprintln\b'),
-        ('SYSTEM', r'\bsystem\.out\.println\b'),
+        ('SYSTEM', r'system\.out\.println'),
         ('PARENIZQ', r'\('),
         ('PARENDER', r'\)'),
         ('LLAVEIZQ', r'\{'),
